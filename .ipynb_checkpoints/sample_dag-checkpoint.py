e735a5ac264bcb48e20969d20858e3512d997960
gretdrg <choiyh1976@gmail.com>
2023-05-17 07:55:11 +0900

    initial uplaod

--- .ipynb_checkpoints/sample_dag-checkpoint.py
+++ .ipynb_checkpoints/sample_dag-checkpoint.py
@@ -4,12 +4,12 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 
 with DAG(
-    dag_id="crossdomain_dm_dag",
+    dag_id="sample_template_dag",
     schedule_interval=None,
     default_args={
         "owner": "OWNER",
         "depends_on_past": False,
-        "start_date": datetime(2023, 5, 18),
+        "start_date": datetime(2022, 7, 1),
         "retries": 0,
     },
     catchup=False,
